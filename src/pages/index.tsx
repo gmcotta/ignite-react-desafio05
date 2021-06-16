ab5cd56f75aa49a74e1b2a19f5a0ec865802b4cc
Gustavo <gmcotta34@gmail.com>
2021-06-16 16:10:07 -0300

    feat: style home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,46 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={styles.homeContainer}>
+      <section className={styles.homePost}>
+        <h2>Como utilizar Hooks</h2>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div>
+          <div>
+            <FiCalendar size={20} />
+            15 Mar 2021
+          </div>
+          <div>
+            <FiUser size={20} />
+            Joseph Oliveira
+          </div>
+        </div>
+      </section>
+      <section className={styles.homePost}>
+        <h2>Criando um app CRA do zero</h2>
+        <p>
+          Tudo sobre como criar a sua primeira aplicação utilizando Create React
+          App.
+        </p>
+        <div>
+          <div>
+            <FiCalendar size={20} />
+            19 Abr 2021
+          </div>
+          <div>
+            <FiUser size={20} />
+            Danilo Vieira
+          </div>
+        </div>
+      </section>
+      <button type="button" className={styles.homeButton}>
+        Carregar mais post
+      </button>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
